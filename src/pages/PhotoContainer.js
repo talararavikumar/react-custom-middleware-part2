94b004a2179eeee95de06e46e503c84bc841caf5
talararavikumar <35607415+talararavikumar@users.noreply.github.com>
2021-04-28 20:03:18 +0530

    without state

--- src/pages/PhotoContainer.js
+++ src/pages/PhotoContainer.js
@@ -2,14 +2,19 @@ import WithLoading from "./PhotosWrapper";
 import photos from "./photos";
 import { connect } from "react-redux";
 import { getPhotos } from "../store/actions";
+import { useState } from "react";
 
 const PhotoWrapper = WithLoading(photos);
 
 function PhotoContainer(props) {
-  const { isLoading, photos } = props;
+  // const { isLoading, photos } = props;
+  const [isLoading, setLoading] = useState(true);
+  const [photos, setPhotos] = useState([]);
 
   return <PhotoWrapper photos={photos} isLoading={isLoading} />;
 }
+
+export default PhotoContainer;
 
 // const mapStatetoProps = (state) => {
 //   isLoading: state.isLoading;
